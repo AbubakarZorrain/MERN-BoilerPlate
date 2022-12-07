4a188d8f8983f8ae4c2bbf719ffe8e9546da8282
AbubakarZorrain <abubakarzorrain71@gmail.com>
2022-12-07 12:29:46 +0500

    Create basic structure for React and Users list

---
+++ client/src/components/users.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default class Users extends React.Component {
+    
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Abubakar</td>
+                </tr>
+            </tbody>
+                
+        </table>
+      </div>
+    );
+  }
+}
