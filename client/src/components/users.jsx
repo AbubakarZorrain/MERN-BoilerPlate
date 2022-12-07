577e3704917fb76667bfe7263daa246fe7ebce51
AbubakarZorrain <abubakarzorrain71@gmail.com>
2022-12-07 12:51:27 +0500

    MUI table is applied.

--- client/src/components/users.jsx
+++ client/src/components/users.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-
+import { Table } from '@mui/material';
+import { TableHead } from '@mui/material';
+import {TableRow} from '@mui/material';
+import { TableBody } from '@mui/material';
+import { TableFooter } from '@mui/material';
+import { TableCell } from '@mui/material';
 export default class Users extends React.Component {
     
   render() {
     return (
       <div>
         <h1>Users</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Username</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Abubakar</td>
-                </tr>
-            </tbody>
-                
-        </table>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Username</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Date Joined</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow>
+                    <TableCell>Abubakar</TableCell> 
+                    <TableCell>abubakarzorrain71@gmail.com</TableCell>
+                    <TableCell>07 Dec 2022</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
       </div>
     );
   }
