e9d5f2a5ccd9b8ad793dfebeea8b0aa714b57383
AbubakarZorrain <abubakarzorrain71@gmail.com>
2023-02-13 00:03:12 +0500

    Write PUT api

--- server/serviceLayer/crud.service.js
+++ server/serviceLayer/crud.service.js
@@ -8,4 +8,8 @@ async function createUser(name,password,email, created_at, updated_at) {
     const createUser = await userModel.create({name:name, email:email, password:password, created_at:created_at, updated_at:updated_at});
     return createUser;
 }
-module.exports = {getUser, createUser}
+async function updateUser(id, name, password, email, created_at, updated_at) {
+    const updateUser = await userModel.findOneAndUpdate({_id:id}, {name:name, email:email, password:password, created_at:created_at, updated_at:updated_at});
+    return updateUser;
+}
+module.exports = {getUser, createUser, updateUser}
